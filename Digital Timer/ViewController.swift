ef1fb6364058a76d304315784675cef886bc4d36
Jonghyun Kim <jhkim@dit.ac.kr>
2018-04-09 15:15:55 +0900

    add Timer closure

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -11,20 +11,40 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
     var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+        // switch off
+        timerSwitch.setOn(false, animated: true)
+        
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
-    @objc func updateTime() {
+    func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss EEE"
+        formatter.dateFormat = "HH:mm:ss a"
         timeLabel.text = formatter.string(from: date)
+//        timeLabel.text = String(describing: date)
     }
 
+    @IBAction func timerStrat(_ sender: Any) {
+        if timerSwitch.isOn == true {
+//            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
 }
 
